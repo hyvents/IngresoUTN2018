9f54d98575f545965a735b678dc29f91a20de0bd
hyvents <36230534+hyvents@users.noreply.github.com>
2018-02-08 12:19:36 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
+    var num1;
+    var num2;
+    var resultado;
+    num1=parseInt(document.getElementById("importe").value);
+    
+    num2=num1;
+    num2=num2/100;
+    num2=num2*25;
+    resultado=num1-num2;
+    document.getElementById("resultado").value=resultado;
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+    var num1;
+    var num2;
+    var num3;
+    var resultado;
+    num1=parseInt(document.getElementById("sueldo").value);
+    
+    num2=num1;
+    num2=num2/100;
+    num2=num2*10;
+    resultado=num1+num2;
+    document.getElementById("resultado").value=resultado;
+
+    
 
 	
 }
